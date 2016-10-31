c87e66512fa1fdc21d98bf36fd34be946de450be
HongSeokJang <deepmindjin@gmail.com>
2016-11-01 01:01:52 +0900

    Added Naver Map View

--- GetSeoulRealEstatePrice/app/src/main/java/com/hongseokandrewjang/android/getseoulrealestateprice/NaverMap.java
+++ GetSeoulRealEstatePrice/app/src/main/java/com/hongseokandrewjang/android/getseoulrealestateprice/NaverMap.java
@@ -10,6 +10,7 @@ import android.view.MotionEvent;
 
 import com.nhn.android.maps.NMapActivity;
 import com.nhn.android.maps.NMapController;
+import com.nhn.android.maps.NMapLocationManager;
 import com.nhn.android.maps.NMapOverlay;
 import com.nhn.android.maps.NMapOverlayItem;
 import com.nhn.android.maps.NMapView;
@@ -26,9 +27,14 @@ public class NaverMap extends NMapActivity{
 
     NMapView mMapView;
     NMapController mMapController;
+    NGeoPoint currentPosition;
+    NMapLocationManager mNMapLocationManager;
+    NMapViewerResourceProvider mNMapViewerResourceProvider;
 
     String sigungu = "";
     boolean isSeoul = false;
+    double pressed_position_x = 0;
+    double pressed_position_y = 0;
 
 
     @Override
@@ -63,7 +69,15 @@ public class NaverMap extends NMapActivity{
         mMapView.setOnMapStateChangeListener(new NMapView.OnMapStateChangeListener() {
             @Override
             public void onMapInitHandler(NMapView nMapView, NMapError nMapError) {
-
+                if (nMapError == null) { // success
+                    mNMapLocationManager = new NMapLocationManager(getBaseContext());
+                    mNMapLocationManager.enableMyLocation(true);
+                    mMapController.setMapCenter(new NGeoPoint(126.978371, 37.5666091), 12);
+                    currentPosition = mNMapLocationManager.getMyLocation();
+//                    mMapController.animateTo(currentPosition);
+                } else { // fail
+                    Log.e(LOG_TAG, "onMapInitHandler: error=" + nMapError.toString());
+                }
             }
 
             @Override
@@ -89,7 +103,10 @@ public class NaverMap extends NMapActivity{
         mMapView.setOnMapViewTouchEventListener(new NMapView.OnMapViewTouchEventListener() {
             @Override
             public void onLongPress(NMapView nMapView, MotionEvent motionEvent) {
-
+                pressed_position_x = motionEvent.getX();
+                pressed_position_y = motionEvent.getY();
+                NMapOverlayItem item = new NMapOverlayItem(new NGeoPoint(pressed_position_x,pressed_position_y),"집값을 알고싶은 지역",null,null);
+                item.setVisibility(NMapOverlayItem.VISIBLE);
             }
 
             @Override
@@ -123,11 +140,19 @@ public class NaverMap extends NMapActivity{
 
 // use map controller to zoom in/out, pan and set map center, zoom level etc.
         mMapController = mMapView.getMapController();
+        mMapView.setBuiltInZoomControls(true,null);
     }
 
     public void onMapInitHandler(NMapView mapView, NMapError errorInfo) {
         if (errorInfo == null) { // success
+            mNMapLocationManager = new NMapLocationManager(getBaseContext());
+            mNMapLocationManager.enableMyLocation(true);
             mMapController.setMapCenter(new NGeoPoint(126.978371, 37.5666091), 11);
+            currentPosition = mNMapLocationManager.getMyLocation();
+            Log.i("CURRENTPOSITION",currentPosition.toString());
+                if (currentPosition!=null)
+                    mMapController.animateTo(currentPosition);
+
         } else { // fail
             Log.e(LOG_TAG, "onMapInitHandler: error=" + errorInfo.toString());
         }
@@ -179,4 +204,5 @@ public class NaverMap extends NMapActivity{
             return null;
         }
     }
+
 }
