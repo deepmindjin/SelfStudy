57bcbbfa3d1d74d057ab9361d5ee25f14200fae7
HongSeokJang <deepmindjin@gmail.com>
2016-11-01 01:03:22 +0900

    Add new Project "FLIPPER!"

---
+++ Flipper/app/src/main/java/com/hongseokandrewjang/android/flipit/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hongseokandrewjang.android.flipit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static String clientID = "DalYdlrJcZb7Wo3bpqBy";
+    public final static String ClientSecreat = "PTt0dsxgB8";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
